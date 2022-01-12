416e5da5a91e652e9a136abc953c014a011d0e42
fagnermeds <fagnermeds@gmail.com>
2022-01-12 11:20:02 -0300

    feat: add feature to create category

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post('/categories', (request, response) => {
+  const { name, description } = request.body;
+
+  const createCategory = new CreateCategoryService(categoriesRepository);
+
+  try {
+    createCategory.execute({ name, description });
+
+    return response.status(201).send();
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+categoriesRoutes.get('/categories', (request, response) => {
+  const categories = categoriesRepository.list();
+
+  return response.json(categories);
+});
+
+export { categoriesRoutes };
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,22 @@
+import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const categoryExists = this.categoriesRepository.findByName(name);
+
+    if (categoryExists) {
+      throw new Error('This category already exists');
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };
